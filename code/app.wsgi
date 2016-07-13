1c005b20578d4250e00a16447db8d1c2d53e6514
Adriano Vieira <adriano.svieira@gmail.com>
2016-07-13 18:18:56 -0300

    Initial commit Flask app over apache-wsgi

---
+++ code/app.wsgi
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import os, time
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello_world():
+    return "Hello, World of Flask on Docker!" + "<br /><small>"+time.strftime("%d/%h/%Y %H:%M:%S")+"<br />@" + os.uname()[1] + "</small>"
---
+++ docker-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "INFO: [docker-compose.sh] build simple flask image"
+if [[ "$HTTP_PROXY" != "http://proxy_not_set:3128" ]]; then
+  ENVIRONMENT="--build-arg https_proxy=$HTTP_PROXY --build-arg http_proxy=$HTTP_PROXY"
+fi
+
+docker build $ENVIRONMENT -t adrianovieira/flask_kafka:debian8 .
