bf7fce6448392b1af4db22ccb964c748726e7e8a
Adriano Vieira <adriano.svieira@gmail.com>
2016-07-20 15:39:53 -0300

    Add script to setup docker proxy

---
+++ docker-setup-proxy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ -f /etc/os-release  ]]; then
+  . /etc/os-release
+fi
+
+if [[ "$ID" == "centos" && "$VERSION_ID" == "7" ]]; then
+
+  if [[ "$HTTP_PROXY" != "http://proxy_not_set:3128" ]]; then
+    echo "INFO: [docker-setup-proxy.sh] setting docker service proxy ($HTTP_PROXY)"
+    sudo mkdir -p /etc/systemd/system/docker.service.d
+    sudo echo "[Service]" > /tmp/proxy.conf
+    sudo echo "Environment='HTTPS_PROXY=$HTTP_PROXY' 'HTTP_PROXY=$HTTP_PROXY'" >> /tmp/proxy.conf
+    sudo mv /tmp/proxy.conf /etc/systemd/system/docker.service.d/proxy.conf
+    sudo systemctl daemon-reload && sudo systemctl restart docker
+  fi
+
+fi
+
+if [[ "$?" == 0 ]]; then
+  echo "INFO: [docker-setup-proxy.sh] finished successfuly"
+else
+  exit 1
+fi
