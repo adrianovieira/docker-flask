a50293db909555bedd6f58420eb1507d8340c0d3
Adriano Vieira <adriano.svieira@gmail.com>
2016-08-10 14:11:19 -0300

    Update image name to build and fix proxy setting for docker environment

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "INFO: [docker-compose.sh] build simple flask image"
-if [[ "$HTTP_PROXY" != "http://proxy_not_set:3128" ]]; then
+if [[ "$HTTP_PROXY" != "" && "$HTTP_PROXY" != "http://proxy_not_set:3128" ]]; then
   ENVIRONMENT="--build-arg https_proxy=$HTTP_PROXY --build-arg http_proxy=$HTTP_PROXY"
 fi
 
-docker build $ENVIRONMENT -t adrianovieira/flask_kafka$2 $1 .
+docker build $ENVIRONMENT -t adrianovieira/flask$2 $1 .
