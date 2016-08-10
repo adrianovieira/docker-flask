dbcebb487988e48f91575041b9975b5c1db8087a
Adriano Vieira <adriano.svieira@gmail.com>
2016-08-10 14:09:50 -0300

    Fix proxy setting bug for docker

--- docker-setup-proxy.sh
+++ docker-setup-proxy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [[ -f /etc/os-release  ]]; then
   . /etc/os-release
@@ -6,11 +6,11 @@ fi
 
 if [[ "$ID" == "centos" && "$VERSION_ID" == "7" ]]; then
 
-  if [[ "$HTTP_PROXY" != "http://proxy_not_set:3128" ]]; then
+  if [[ "$HTTP_PROXY" != "" && "$HTTP_PROXY" != "http://proxy_not_set:3128" ]]; then
     echo "INFO: [docker-setup-proxy.sh] setting docker service proxy ($HTTP_PROXY)"
     sudo mkdir -p /etc/systemd/system/docker.service.d
     sudo echo "[Service]" > /tmp/proxy.conf
-    sudo echo "Environment='HTTPS_PROXY=$HTTP_PROXY' 'HTTP_PROXY=$HTTP_PROXY'" >> /tmp/proxy.conf
+    sudo echo "Environment='HTTPS_PROXY=$HTTP_PROXY' 'HTTP_PROXY=$HTTP_PROXY' 'no_proxy=\"127.0.0.1, localhost\"'" >> /tmp/proxy.conf
     sudo mv /tmp/proxy.conf /etc/systemd/system/docker.service.d/proxy.conf
     sudo systemctl daemon-reload && sudo systemctl restart docker
   fi
